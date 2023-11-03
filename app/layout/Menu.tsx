e64e2cc494fbc2308f8b47d8e8859bfa72e22881
jsbadz <johnstephen.sb@gmail.com>
2023-11-03 23:12:31 +0800

    chore: home page

--- app/layout/Menu.tsx
+++ app/layout/Menu.tsx
@@ -1,7 +1,82 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { FiSearch } from "react-icons/fi";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export const Menu = () => {
+  // * comment
+  // ? comment
+  // ! comment
+  // TODO: comment
+
+  const subMenu = [
+    { name: "Best Seller", url: "" },
+    { name: "About Us", url: "" },
+    { name: "Message Us", url: "" },
+    { name: "Feedback?", url: "" },
+  ];
+
+  const menu = [{ name: "Shoes", url: "" }];
+
   return (
-    <div className="text-red underline">Menu</div>
-  )
-}
+    <Fragment>
+      <header className="border-b bg-white font-sans">
+        <section className="flex sm:items-center relative px-10 min-h-[75px] bg-header drop-shadow-xl ">
+          <Link href={"/"}>
+            <Image
+              src="/images/logo.png"
+              width={150}
+              height={150}
+              alt="logo"
+              priority
+            />
+          </Link>
+
+          <ul className="flex space-x-8 lg:absolute lg:top-2/4 lg:left-2/4 lg:-translate-x-1/2 lg:-translate-y-1/2 max-lg:hidden">
+            {subMenu.map((item, index) => (
+              <li>
+                <Link
+                  key={index}
+                  href={item.url}
+                  className="font-bold text-white hover:text-light-theme text-[15px]"
+                >
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="lg:absolute lg:right-10 flex items-center ml-auto max-lg:justify-end">
+            <div className="flex ml-auto lg:order-2">
+              <button className="hover:text-light-theme">
+                <FiSearch size={25} />
+              </button>
+              <button className="ml-7 hover:text-light-theme lg:hidden">
+                <RxHamburgerMenu size={25} />
+              </button>
+            </div>
+          </div>
+        </section>
+        <div className="flex flex-wrap items-center py-1 relative drop-shadow-xl ">
+          <ul className="flex justify-center lg:space-x-8 max-lg:space-y-2 max-lg:block w-full">
+            {Array.from({ length: 10 }, (_, idx) => (
+              <>
+                {menu.map((item, index) => (
+                  <li className="max-lg:py-2">
+                    <Link
+                      key={index}
+                      href={item.url}
+                      className="lg:hover:text-light-theme font-bold text-dark-theme text-[14px] block"
+                    >
+                      {item.name}
+                    </Link>
+                  </li>
+                ))}
+              </>
+            ))}
+          </ul>
+        </div>
+      </header>
+    </Fragment>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,36 @@
-import Image from 'next/image'
+import Image from "next/image";
+import noImage from "../public/images/no-image.webp";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="text-red underline">
-        Deal Click
+    <main className="flex min-h-screen flex-col">
+      {/* // ! Banner */}
+      <div className="hero-image">
+        <div className="hero-text inline-block">
+          <button className="font-[700] hover:bg-light-theme hover:text-dark-theme text-[40px] rounded-tl-3xl rounded-br-3xl hover:rounded-tl-none hover:rounded-br-none hover:rounded-tr-3xl hover:rounded-bl-3xl transition duration-300 ease-in-out text-bold bg-dark-theme text-light-theme py-4 px-8 drop-shadow-2xl ">
+            Click Me!
+          </button>
+        </div>
+      </div>
+      {/* // * Content */}
+      <div className="flex items-center w-full justify-between py-16 container mx-auto px-4">
+        {Array.from({ length: 3 }, (_, idx) => (
+          <div className="border-black border-2 p-16 flex flex-col text-center">
+            <span>LOGO</span>
+            Click Me!
+          </div>
+        ))}
+      </div>
+      <div className="bg-[#f6f6f6]">
+        <div className="grid grid-cols-3 gap-4 py-16 container mx-auto px-auto">
+          {Array.from({ length: 9 }, (_, idx) => (
+            <div className="border-black border-2 p-16 flex flex-col text-center">
+              <span>LOGO</span>
+              Click Me!
+            </div>
+          ))}
+        </div>
       </div>
     </main>
-  )
+  );
 }
